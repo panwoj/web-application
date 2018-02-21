aa57ff78fba5217e6d675985557d3d1b21769841
panwoj <pan_woj@poczta.onet.pl>
2018-02-21 06:37:16 +0100

    21.1 first commit

--- src/main/java/com/crud/tasks/mapper/TrelloMapper.java
+++ src/main/java/com/crud/tasks/mapper/TrelloMapper.java
@@ -10,24 +10,24 @@ import static java.util.stream.Collectors.toList;
 @Component
 public class TrelloMapper {
 
-    public List<TrelloBoard> mapToBoards(final List<TrelloBoardDto> trelloBoardDto) {
-        return trelloBoardDto.stream()
+    public List<TrelloBoard> mapToBoards(final List<TrelloBoardDto> trelloBoardsDto) {
+        return trelloBoardsDto.stream()
                 .map(trelloBoard ->
                         new TrelloBoard(trelloBoard.getId(), trelloBoard.getName(), mapToList(trelloBoard.getLists())))
-                .collect(toList());
-    }
-
-    public List<TrelloList> mapToList (final List<TrelloListDto> trelloListDto) {
-        return trelloListDto.stream()
-                .map(trelloList -> new TrelloList(trelloList.getId(), trelloList.getName(), trelloList.isClosed()))
                 .collect(toList());
     }
 
     public List<TrelloBoardDto> mapToBoardsDto(final List<TrelloBoard> trelloBoards) {
         return trelloBoards.stream()
                 .map(trelloBoard ->
-                    new TrelloBoardDto(trelloBoard.getId(), trelloBoard.getName(),
-                            mapToListDto(trelloBoard.getLists()))).collect(toList());
+                        new TrelloBoardDto(trelloBoard.getId(), trelloBoard.getName(),
+                                mapToListDto(trelloBoard.getLists()))).collect(toList());
+    }
+
+    public List<TrelloList> mapToList (final List<TrelloListDto> trelloListDto) {
+        return trelloListDto.stream()
+                .map(trelloList -> new TrelloList(trelloList.getId(), trelloList.getName(), trelloList.isClosed()))
+                .collect(toList());
     }
 
     public List<TrelloListDto> mapToListDto(final List<TrelloList> trelloLists) {
--- src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTest.java
+++ src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTest.java
@@ -1,10 +1,81 @@
 package com.crud.tasks.trello.mapper;
 
+import com.crud.tasks.domain.*;
+import com.crud.tasks.mapper.TrelloMapper;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TrelloMapperTest {
-    @Test
-    public void test() {
 
+    @Mock
+    private TrelloListDto trelloListDto;
+
+    @Autowired
+    TrelloMapper trelloMapper;
+
+    private List<TrelloListDto> trelloListDtoList = new ArrayList<>();
+    private List<TrelloBoardDto> trelloBoardDtoList = new ArrayList<>();
+
+
+    @Before
+    public void setup() {
+        trelloListDtoList.add(trelloListDto);
+        trelloBoardDtoList.add(new TrelloBoardDto("1", "name", trelloListDtoList));
     }
+
+    @Test
+    public void testmapToBoards() {
+        //Given
+        //When
+        List<TrelloBoard> trelloBoards = trelloMapper.mapToBoards(trelloBoardDtoList);
+        //Then
+        Assert.assertEquals(trelloBoardDtoList.get(0).getId(), trelloBoards.get(0).getId());
+        Assert.assertEquals(trelloBoardDtoList.get(0).getName(), trelloBoards.get(0).getName());
+        Assert.assertEquals(trelloBoardDtoList.get(0).getLists().size(), trelloBoards.get(0).getLists().size());
+    }
+
+//    @Test
+//    public void test() {
+//        //Given
+//        //When
+//        //Then
+//    }
+//
+//    @Test
+//    public void test() {
+//        //Given
+//        //When
+//        //Then
+//    }
+//
+//    @Test
+//    public void test() {
+//        //Given
+//        //When
+//        //Then
+//    }
+//
+//    @Test
+//    public void test() {
+//        //Given
+//        //When
+//        //Then
+//    }
+//
+//    @Test
+//    public void test() {
+//        //Given
+//        //When
+//        //Then
+//    }
 }
