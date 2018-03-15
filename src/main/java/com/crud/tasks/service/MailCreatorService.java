290b220e50a47cc97f7801f0ef688b84f428444d
panwoj <pan_woj@poczta.onet.pl>
2018-03-15 15:34:43 +0100

    Assignment 24.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -20,9 +20,14 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("short_message", "PREVIEW: " + message.substring(0, 10) + "...");
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "Bye!");
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("company_phone", adminConfig.getCompanyPhone());
+        context.setVariable("company_mail", adminConfig.getCompanyEmail());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
